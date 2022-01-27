46baed3813d5728dae630a780e09a537aab29d0a
Abdul Wahid bin Ani <abdul_wahid1986@yahoo.com>
2022-01-28 01:30:01 +0800

    Update backup.sh
    Kenalpasti folder script

--- backup.sh
+++ backup.sh
@@ -2,20 +2,21 @@
 # Disediakan oleh Abdul Wahid bin Ani
 # Skrip backup untuk web yang diuruskan menggunakan RUNCLOUD
 
-source config
+BASEDIR=$(dirname "$0")
+source $BASEDIR/config
 
 find $BACKUP_DIR/ -iname "$BACKUP_SUFFIX-*.tgz" -mtime +$SAVE_BACKUP_DURATION -exec rm -rf {} \;
 
-cd $SCRIPT_FOLDER/setbackup;
+cd $BASEDIR/setbackup;
 
 for i in $(ls -1 *.conf);do
-	SYSTEM_USER=$(head -n 1 $SCRIPT_FOLDER/setbackup/$i);
-	WEB_FOLDER=$(head -n 2 $SCRIPT_FOLDER/setbackup/$i|tail -n 1);
-	LOCAL_DB=$(head -n 3 $SCRIPT_FOLDER/setbackup/$i|tail -n 1);
-	DB_NAME=$(head -n 4 $SCRIPT_FOLDER/setbackup/$i|tail -n 1);	
-	DB_HOST=$(head -n 5 $SCRIPT_FOLDER/setbackup/$i|tail -n 1);
-	DB_USER=$(head -n 6 $SCRIPT_FOLDER/setbackup/$i|tail -n 1);
-	DB_PASS=$(head -n 7 $SCRIPT_FOLDER/setbackup/$i|tail -n 1);
+	SYSTEM_USER=$(head -n 1 $BASEDIR/setbackup/$i);
+	WEB_FOLDER=$(head -n 2 $BASEDIR/setbackup/$i|tail -n 1);
+	LOCAL_DB=$(head -n 3 $BASEDIR/setbackup/$i|tail -n 1);
+	DB_NAME=$(head -n 4 $BASEDIR/setbackup/$i|tail -n 1);	
+	DB_HOST=$(head -n 5 $BASEDIR/setbackup/$i|tail -n 1);
+	DB_USER=$(head -n 6 $BASEDIR/setbackup/$i|tail -n 1);
+	DB_PASS=$(head -n 7 $BASEDIR/setbackup/$i|tail -n 1);
 	
 	mkdir -p $BACKUP_DIR/$WEB_FOLDER;
 	OUTPUT=${BACKUP_PREFIX}${WEB_FOLDER}${BACKUP_SUFFIX};
